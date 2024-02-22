9b976025a73ec1f44e7e1435ea89008a102aaef3
Yuttasak(Yo)Pannawat <yuttasak.pan@sellsuki.com>
2024-02-22 14:01:01 +0700

    feat:🥷 (workshop) waitgroup

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg = sync.WaitGroup{}
+	count := 5
+	groupCount := 2
+	wg.Add(groupCount)
+	go group1(&wg, count)
+	go group2(&wg, count)
+	wg.Wait()
+}
+
+func group1(wg *sync.WaitGroup, n int) {
+	defer wg.Done()
+	for i := 0; i < n; i++ {
+		fmt.Println("Sended ", i)
+	}
+}
+
+func group2(wg *sync.WaitGroup, n int) {
+	defer wg.Done()
+	for i := 0; i < n; i++ {
+		fmt.Println("Received ", i)
+	}
+}
